6bae07d7e1bddfbf021655675345642ea7029ead
joe <kaihua.ckh@gmail.com>
2020-11-02 20:23:35 +0800

    add part6

---
+++ concurrent/src/main/java/com/joe/concurrent/part6/Renderer.java
@@ -0,0 +1,57 @@
+package com.joe.concurrent.part6;
+
+import java.util.List;
+import java.util.concurrent.*;
+
+import static com.joe.concurrent.utils.LaunderThrowable.launderThrowable;
+
+/**
+ * Renderer
+ * <p/>
+ * Using CompletionService to render page elements as they become available
+ *
+ * @author Brian Goetz and Tim Peierls
+ */
+public abstract class Renderer {
+    private final ExecutorService executor;
+
+    Renderer(ExecutorService executor) {
+        this.executor = executor;
+    }
+
+    void renderPage(CharSequence source) {
+        final List<ImageInfo> info = scanForImageInfo(source);
+        CompletionService<ImageData> completionService =
+                new ExecutorCompletionService<>(executor);
+        for (final ImageInfo imageInfo : info)
+            completionService.submit(imageInfo::downloadImage);
+
+        renderText(source);
+
+        try {
+            for (int t = 0, n = info.size(); t < n; t++) {
+                Future<ImageData> f = completionService.take();
+                ImageData imageData = f.get();
+                renderImage(imageData);
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } catch (ExecutionException e) {
+            throw launderThrowable(e.getCause());
+        }
+    }
+
+    interface ImageData {
+    }
+
+    interface ImageInfo {
+        ImageData downloadImage();
+    }
+
+    abstract void renderText(CharSequence s);
+
+    abstract List<ImageInfo> scanForImageInfo(CharSequence s);
+
+    abstract void renderImage(ImageData i);
+
+}
