159ebb76a41742aaee38ec4036b4707226507d64
joe <kaihua.ckh@gmail.com>
2020-11-03 09:03:42 +0800

    leetcode

--- algorithm/src/main/java/com/joe/leetcode/part0/OpenLock.java
+++ algorithm/src/main/java/com/joe/leetcode/part0/OpenLock.java
@@ -1,5 +1,7 @@
 package com.joe.leetcode.part0;
 
+import java.util.*;
+
 /**
  * 752. Open the Lock
  *
@@ -8,12 +10,74 @@ package com.joe.leetcode.part0;
  */
 public class OpenLock {
 
-    public int openLock(String[] deadends, String target) {
+    // 将 s[j] 向上拨动一次
+    String plusOne(String s, int j) {
+        char[] ch = s.toCharArray();
+        if (ch[j] == '9')
+            ch[j] = '0';
+        else
+            ch[j] += 1;
+        return new String(ch);
+    }
 
+    // 将 s[i] 向下拨动一次
+    String minusOne(String s, int j) {
+        char[] ch = s.toCharArray();
+        if (ch[j] == '0')
+            ch[j] = '9';
+        else
+            ch[j] -= 1;
+        return new String(ch);
+    }
+
+    public int openLock(String[] deadends, String target) {
+        // 记录需要跳过的死亡密码
+        Set<String> deads = new HashSet<>(Arrays.asList(deadends));
+        // 记录已经穷举过的密码，防止走回头路
+        Set<String> visited = new HashSet<>();
+        Queue<String> q = new LinkedList<>();
+        // 从起点开始启动广度优先搜索
+        int step = 0;
+        q.offer("0000");
+        visited.add("0000");
+
+        while (!q.isEmpty()) {
+            int sz = q.size();
+            /* 将当前队列中的所有节点向周围扩散 */
+            for (int i = 0; i < sz; i++) {
+                String cur = q.poll();
+
+                /* 如果在死亡队列中, 则跳过 */
+                if (deads.contains(cur)) continue;
+                /* 判断是否到达终点 */
+                if (target.equals(cur)) return step;
+
+                /* 将一个节点的未遍历相邻节点加入队列 */
+                for (int j = 0; j < 4; j++) {
+                    String up = plusOne(cur, j);
+                    if (!visited.contains(up)) {
+                        q.offer(up);
+                        visited.add(up);
+                    }
+                    String down = minusOne(cur, j);
+                    if (!visited.contains(down)) {
+                        q.offer(down);
+                        visited.add(down);
+                    }
+                }
+            }
+            /* 在这里增加步数 */
+            step++;
+        }
+        // 如果穷举完都没找到目标密码，那就是找不到了
         return -1;
     }
 
     public static void main(String[] args) {
+        String[] deadends = {"0000"};
+//        String[] deadends = {"0201","0101","0102","1212","2002"};
+        System.out.println(new OpenLock().openLock(deadends, "8888"));
+
 
     }
 
