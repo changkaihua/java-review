ed500155aa48c623b56161a0b0d56686cdf8ffe6
JoeMendezCKH <kaihua.ckh@gmail.com>
2020-11-27 11:12:26 +0800

    leetcode

--- algorithm/src/main/java/com/joe/leetcode/tree/InvertTree.java
+++ algorithm/src/main/java/com/joe/leetcode/tree/InvertTree.java
@@ -1,6 +1,7 @@
 package com.joe.leetcode.tree;
 
 import com.joe.leetbook.tree.TreeNode;
+import com.joe.leetbook.tree.TreeTraverse;
 import org.junit.Test;
 
 /**
@@ -9,11 +10,24 @@ import org.junit.Test;
  */
 public class InvertTree {
     public TreeNode invertTree(TreeNode root) {
-        return null;
+        if (root == null) return null;
+        TreeNode left = invertTree(root.left);
+        root.left = invertTree(root.right);
+        root.right = left;
+        return root;
     }
 
     @Test
     public void test() {
+        TreeTraverse treeTraverse = new TreeTraverse();
+        // 4,2,7,1,3,6,9
+        TreeNode root = new TreeNode(4,
+                new TreeNode(2, new TreeNode(1), new TreeNode(3)),
+                new TreeNode(7, new TreeNode(6), new TreeNode(9)));
+
+        System.out.println("before" + treeTraverse.preorderTraversal(root) + "\n");
+        invertTree(root);
+        System.out.println(treeTraverse.preorderTraversal(root));
 
     }
 }
---
+++ algorithm/src/main/java/com/joe/leetcode/tree/LowestCommonAncestorTree.java
@@ -0,0 +1,10 @@
+package com.joe.leetcode.tree;
+
+/**
+ * 235. Lowest Common Ancestor of a Binary Search Tree
+ *
+ * @author ckh
+ * @since 11/27/2020
+ */
+public class LowestCommonAncestorTree {
+}
