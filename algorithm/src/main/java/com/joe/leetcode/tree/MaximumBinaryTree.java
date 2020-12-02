150ab2a2d2429a9a4d77aa1c6b162794b62d5062
JoeMendezCKH <kaihua.ckh@gmail.com>
2020-12-02 11:20:28 +0800

    leetcode

--- algorithm/src/main/java/com/joe/leetcode/tree/MaximumBinaryTree.java
+++ algorithm/src/main/java/com/joe/leetcode/tree/MaximumBinaryTree.java
@@ -15,7 +15,26 @@ public class MaximumBinaryTree {
             2. left tree is constructMaximumBinaryTree(0, maxValueIndex-1)
             3. right tree is constructMaximumBinaryTree(maxValueIndex+1,nums.length-1)
          */
-        return null;
+        return constructMaximumBinaryTree(nums, 0, nums.length - 1);
+    }
 
+    private TreeNode constructMaximumBinaryTree(int[] nums, int start, int end) {
+
+        if (start > end) {
+            return null;
+        }
+
+        int max = nums[start];
+        int idx = start;
+        for (int i = start; i <= end; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
+                idx = i;
+            }
+        }
+        TreeNode root = new TreeNode(max);
+        root.left = constructMaximumBinaryTree(nums, start, idx - 1);
+        root.right = constructMaximumBinaryTree(nums, idx + 1, end);
+        return root;
     }
 }
