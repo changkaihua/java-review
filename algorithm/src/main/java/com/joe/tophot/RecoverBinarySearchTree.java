c754380d6c57e6e3686290a4d1b4a68c491516de
JoeMendezCKH <kaihua.ckh@gmail.com>
2020-12-05 09:34:05 +0800

    leetcode

--- algorithm/src/main/java/com/joe/tophot/RecoverBinarySearchTree.java
+++ algorithm/src/main/java/com/joe/tophot/RecoverBinarySearchTree.java
@@ -1,7 +1,10 @@
 package com.joe.tophot;
 
 import com.joe.leetbook.tree.TreeNode;
+import com.sun.media.sound.RIFFInvalidDataException;
+import org.junit.Test;
 
+import java.nio.channels.MulticastChannel;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -52,4 +55,54 @@ public class RecoverBinarySearchTree {
         list.add(root.val);
         inorder(root.right, list);
     }
+
+
+    private TreeNode wrong1, wrong2, prior;
+
+    public void recoverTree(TreeNode root) {
+        inorder(root);
+        int temp = wrong1.val;
+        wrong1.val = wrong2.val;
+        wrong2.val = temp;
+    }
+
+    private void inorder(TreeNode root) {
+        if (root == null) return;
+        inorder(root.left);
+
+        if (null != prior && prior.val > root.val) {
+            if (wrong1 == null) {
+                wrong1 = prior;
+            }
+            wrong2 = root;
+        }
+        if (prior != null && wrong2 == prior && wrong1.val < root.val) {
+            return;
+        }
+        prior = root;
+        inorder(root.right);
+    }
+
+
+    @Test
+    public void test() {
+//        TreeNode root = new TreeNode(3, new TreeNode(1),
+//                new TreeNode(4, new TreeNode(2), new TreeNode(6)));
+//        TreeNode root = new TreeNode(1, new TreeNode(3, null, new TreeNode(2)), null);
+//
+        TreeNode root = new TreeNode(6,
+                new TreeNode(8, new TreeNode(2), new TreeNode(7)),
+                new TreeNode(15, new TreeNode(14), new TreeNode(18)));
+
+
+        recoverTree(root);
+        /*
+         *
+         *           8
+         *       10        9
+         *      2 1       11   12
+         * */
+    }
+
+
 }
