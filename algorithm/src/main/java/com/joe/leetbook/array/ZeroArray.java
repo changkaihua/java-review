3d4ead005ffa69b9bb5aa7b61b7d3de4107ec738
JoeMendezCKH <kaihua.ckh@gmail.com>
2020-12-09 21:26:46 +0800

    leetbook array

--- algorithm/src/main/java/com/joe/leetbook/array/ZeroArray.java
+++ algorithm/src/main/java/com/joe/leetbook/array/ZeroArray.java
@@ -5,9 +5,12 @@ import org.junit.Test;
 import java.util.Arrays;
 
 /**
+ * 面试题 01.08. 零矩阵
+ *
  * @author ckh
  * @since 2020/12/9
  */
+@SuppressWarnings("ALL")
 public class ZeroArray {
 
     public void setZeroes(int[][] matrix) {
@@ -17,24 +20,24 @@ public class ZeroArray {
         int row = matrix.length;
         int col = matrix[0].length;
         boolean[] flag = new boolean[col];
+
         for (int i = 0; i < row; i++) {
+            boolean rowNeedZero = false;
             for (int j = 0; j < col; j++) {
-                if (flag[j]) {
-                    matrix[i][j] = 0;
-                    continue;
-                }
                 if (matrix[i][j] == 0) {
                     flag[j] = true;
-                    for (int t = 0; t < col; t++) {
-                        matrix[i][t] = 0;
-                    }
-                    break;
+                    rowNeedZero = true;
+                }
+            }
+            if (rowNeedZero) {
+                for (int t = 0; t < col; t++) {
+                    matrix[i][t] = 0;
                 }
             }
         }
 
         for (int i = 0; i < flag.length; i++) {
-            if (flag[i]){
+            if (flag[i]) {
                 for (int j = 0; j < row; j++) {
                     matrix[j][i] = 0;
                 }
@@ -43,12 +46,12 @@ public class ZeroArray {
     }
 
     @Test
-    public void test(){
+    public void test() {
         int[][] matrix = {
                 //[[1,1,1],[1,0,1],[1,1,1]]
-                {0,1,2,0},
-                {3,4,5,6},
-                {1,3,1,5}
+                {0, 1, 2, 0},
+                {3, 4, 5, 6},
+                {1, 3, 1, 5}
         };
         setZeroes(matrix);
 
