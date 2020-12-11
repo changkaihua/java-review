dd69c036c24df3c282b9b51b1bda35629b147cd0
JoeMendezCKH <kaihua.ckh@gmail.com>
2020-12-11 16:15:37 +0800

    leetcode

---
+++ algorithm/src/main/java/com/joe/leetcode/daily/Dota2Senate.java
@@ -0,0 +1,46 @@
+package com.joe.leetcode.daily;
+
+import com.sun.org.apache.bcel.internal.generic.DSUB;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 649. Dota2 Senate
+ *
+ * @author ckh
+ * @since 2020/12/11
+ */
+public class Dota2Senate {
+
+    /**
+     * Radiant  Dire
+     */
+    public String predictPartyVictory(String senate) {
+        /*ArrayList<Integer> R = new ArrayList<>();
+        ArrayList<Integer> D = new ArrayList<>();
+        int RSum = 0, DSum = 0;
+        for (int i = 0; i < senate.toCharArray().length; i++) {
+            if (senate.charAt(i) == 'R') {
+                R.add(i);
+                RSum += i;
+            } else {
+                D.add(i);
+                DSum += i;
+            }
+        }
+        if (R.size() > D.size()) {
+            return "Radiant";
+        } else if (R.size() < D.size()) {
+            return "Dire";
+        } else {
+            if (RSum < DSum) {
+                return "Radiant";
+            } else {
+                return "Dire";
+            }
+        }*/
+
+
+    }
+}
