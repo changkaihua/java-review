756a0bc91e5e2720e4da324482bf6e5ae40c6908
JoeMendezCKH <kaihua.ckh@gmail.com>
2021-01-07 10:54:47 +0800

    leetcode

--- algorithm/src/main/java/com/joe/leetcode/daily/NumberOfProvinces.java
+++ algorithm/src/main/java/com/joe/leetcode/daily/NumberOfProvinces.java
@@ -1,5 +1,8 @@
 package com.joe.leetcode.daily;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * 547. 省份数量
  *
@@ -36,5 +39,35 @@ public class NumberOfProvinces {
             }
         }
     }
+
+    public int findCircleNumV2_BFS(int[][] isConnected) {
+        // int[][] isConnected 是无向图的邻接矩阵，n 为无向图的顶点数量
+        int n = isConnected.length;
+        // 定义 boolean 数组标识顶点是否被访问
+        boolean[] visited = new boolean[n];
+
+        // 定义 cnt 来累计遍历过的连通域的数量
+        int cnt = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            // 若当前顶点 i 未被访问，说明又是一个新的连通域，则bfs新的连通域且cnt+=1.
+            if (!visited[i]) {
+                cnt++;
+                queue.offer(i);
+                visited[i] = true;
+                while (!queue.isEmpty()) {
+                    int v = queue.poll();
+                    for (int w = 0; w < n; w++) {
+                        if (isConnected[v][w] == 1 && !visited[w]) {
+                            visited[w] = true;
+                            queue.offer(w);
+                        }
+                    }
+                }
+            }
+        }
+        return cnt;
+    }
+
 
 }
